363334f6bc5fcf8361200bc236a3b9b3c42971d7
make711 <make711@github.com>
2016-04-21 00:49:11 -0700

    render event list

--- js/main.js
+++ js/main.js
@@ -14,27 +14,32 @@ function Day (dm, dw, m, cm) {
 	this.events = [];
 };
 
-function Event (eventName, evenType, startTime, endTime) {
-	this.eventName = eventName;
-	this.evenType = evenType;
-	this.startTime = startTime;
-	this.endTime = endTime;
+function Event (id, name, type, start, end, notes) {
+	this.id = id;
+	this.name = name;
+	this.type = type;
+	this.start = start;
+	this.end = end;
 	this.notes = notes;
 }
 
 var month = [];
+var selectedDay = {
+	week: -1,
+	day: -1
+}
 
-Date.prototype.monthDays= function(){
+Date.prototype.monthDays= function() {
     var d= new Date(this.getFullYear(), this.getMonth()+1, 0);
     return d.getDate();
 }
 
-Date.prototype.firstDayOfMonth= function(){
+Date.prototype.firstDayOfMonth= function() {
     var d= new Date(this.getFullYear(), this.getMonth(), 1);
     return d.getDay();
 }
 
-var renderNavElement = function () {
+var renderNavElement = function() {
 	var navDOM = document.getElementById('nav');
 	var monthNavDOM = document.getElementById('month-nav');
 
@@ -56,7 +61,7 @@ var renderNavElement = function () {
 	renderMonthLabel();
 }
 
-var renderMonthLabel = function () {
+var renderMonthLabel = function() {
 	var currentMonth = MONTH_LABEL[selectedDate.getMonth()];
 	var currentYear = selectedDate.getFullYear();
 	var monthAndYearLabel = currentMonth + ", " + currentYear;
@@ -67,7 +72,7 @@ var renderMonthLabel = function () {
 	monthLabelElement.appendChild(document.createTextNode(monthAndYearLabel));
 }
 
-var renderCalendar = function () {
+var renderCalendar = function() {
 	var mainContainer = document.getElementById('main-container');
 	var calendarContainer = document.createElement('div');
 	calendarContainer.setAttribute('id', 'calendar-container');
@@ -78,7 +83,7 @@ var renderCalendar = function () {
 	renderMonthBody();
 }
 
-var renderCalendarHeader = function () {
+var renderCalendarHeader = function() {
 	var calendarContainer = document.getElementById('calendar-container');
 	var headRow = document.createElement('div');
 	headRow.setAttribute('id', 'header-row');
@@ -89,7 +94,7 @@ var renderCalendarHeader = function () {
 	calendarContainer.insertBefore(headRow, calendarContainer.firstChild);
 }
 
-var renderCalendarHeaderColumn = function (dayNumber) {
+var renderCalendarHeaderColumn = function(dayNumber) {
 	var headerColumn = document.createElement('div');
 
 	if(dayNumber === 0 || dayNumber === 6) {
@@ -103,7 +108,7 @@ var renderCalendarHeaderColumn = function (dayNumber) {
 	return headerColumn;
 }
 
-var renderMonthBody = function () {
+var renderMonthBody = function() {
 	buildMonthDays();
 	var calendarContainer = document.getElementById('calendar-container');
 	var calendarBody = document.createElement("div");
@@ -121,7 +126,7 @@ var renderMonthBody = function () {
 	})
 }
 
-var buildMonthDays = function () {
+var buildMonthDays = function() {
 	var numberOfDays = selectedDate.monthDays();
 	var lastMonthDate = new Date(selectedDate);
 	lastMonthDate.setMonth(lastMonthDate.getMonth() - 1);
@@ -159,7 +164,7 @@ var buildMonthDays = function () {
 	}
 }
 
-var createDayDOM = function (day) {
+var createDayDOM = function(day) {
 	var dayDOM = document.createElement('div');
 	var today = new Date();
 
@@ -182,79 +187,169 @@ var createDayDOM = function (day) {
 
 	dayDOM.innerHTML = "<span class='day-number'>"+ day.dayOfMonth + "</span>";
 
-
 	return dayDOM;
 }
 
-var mouseOverDayDOM = function (ele) {
+var mouseOverDayDOM = function(ele) {
 	var dayNumberDOM = ele.getElementsByClassName('day-number');
 	var thisDay = dayNumberDOM[0].textContent;
 }
 
-var mouseClickDayDOM = function (ele) {
+var mouseClickDayDOM = function(ele) {
 	var day = month[ele.week][ele.day];
+	selectedDay.week = ele.week;
+	selectedDay.day = ele.day;
 	renderEventListDOM(day);
 }
 
-var appendDayElement = function (date, day, currentMonth) {
+var appendDayElement = function(date, day, currentMonth) {
 	var calendarBody = document.getElementById("calendar-body");
 	calendarBody.appendChild(renderDayElement(date, day, currentMonth));
 }
 
-var previousMonthClick = function () {
+var previousMonthClick = function() {
 	selectedDate.setMonth(selectedMonth() - 1);
 	renderMonthLabel();
 	renderMonthBody();
 }
 
-var nextMonthClick = function () {
+var nextMonthClick = function() {
 	selectedDate.setMonth(selectedMonth() + 1);
 	renderMonthLabel();
 	renderMonthBody();
 }
 
-var monthLabelClick = function () {
+var monthLabelClick = function() {
 	resetDOM();
 	renderCalendar();
 }
 
-var renderEventListDOM = function (day) {
+var renderEventListDOM = function(day) {
 	resetDOM();
 	var eventListContainer = document.getElementById('event-list-container');
 	var eventList = document.createElement("div");
 
-	eventList.setAttribute('class', 'event-list');
+	eventList.setAttribute('id', 'event-list');
 
 	eventListContainer.appendChild(eventList);
 
 	var monthNavDOM = document.getElementById('month-nav');
 
-	if(day.events.length) {
+	if(day.events) {
 		day.events.forEach(function(event) {
-			renderEventDOM(event);
+			eventList.appendChild(renderEventDOM(event));
 		});
 	} else {
 		eventList.innerHTML = "";
 		eventList.appendChild(document.createTextNode("There is no event."));
 	}
+
 	renderEventViewNav();
 }
 
-var renderEventDOM = function (event) {
+var renderEventDOM = function(event) {
+	var thisEventDOM = document.createElement('div');
+	thisEventDOM.setAttribute('id', 'event'+event.id);
+	thisEventDOM.setAttribute('class', 'event');
+	var title = document.createElement('div');
+	var time = document.createElement('div');
+	var notes = document.createElement('div');
 
+	title.setAttribute('class', 'event-title');
+
+	title.appendChild(document.createTextNode(event.name));
+	if(event.start && event.end) {
+		time.appendChild(document.createTextNode(event.start + ' - ' + event.end));
+	}
+	if(event.notes) {
+		notes.appendChild(document.createTextNode(event.notes));
+	} else {
+		notes.appendChild(document.createTextNode("no notes"));
+	}
+
+	thisEventDOM.appendChild(notes);
+	thisEventDOM.insertBefore(time, notes);
+	thisEventDOM.insertBefore(title, time);
+	return thisEventDOM;
 }
 
-var renderEventViewNav = function (day) {
+var renderEventViewNav = function(day) {
 	var newEvent = document.getElementById("new-event");
 	var newEventButton = document.createElement('button');
 	newEventButton.setAttribute('type', 'button');
-	newEventButton.setAttribute('onclick', 'addEventClick()');
+	newEventButton.setAttribute('onclick', 'newEventClick(this)');
+	newEventButton.setAttribute('id', 'modal_open');
 	newEventButton.appendChild(document.createTextNode('New event'));
 
 	newEvent.appendChild(newEventButton);
 }
 
-var resetDOM = function () {
+//The code for the new event modal is edited based on a online tutorial 
+//source: http://www.the-art-of-web.com/javascript/feedback-modal-window/
+
+var newEventClick = function(e) {
+	var modalWrapper = document.getElementById("modal-wrapper");
+    var modalWindow  = document.getElementById("modal-window");
+
+
+	modalWrapper.className = "overlay";
+	  var overflow = modalWindow.offsetHeight - document.documentElement.clientHeight;
+	  if(overflow > 0) {
+	    modalWindow.style.maxHeight = (parseInt(window.getComputedStyle(modalWindow).height) - overflow) + "px";
+	  }
+	  modalWindow.style.marginTop = (-modalWindow.offsetHeight)/2 + "px";
+	  modalWindow.style.marginLeft = (-modalWindow.offsetWidth)/2 + "px";
+	  e.preventDefault ? e.preventDefault() : e.returnValue = false;
+}
+
+var modalCloseClick = function(e) {
+	var modalWrapper = document.getElementById("modal-wrapper");
+	modalWrapper.className = "";
+    e.preventDefault ? e.preventDefault() : e.returnValue = false;
+}
+
+var addEventClick = function() {
+	var name = document.getElementById('new-event-name').value;
+
+	if(!name) {
+		alert("Missing event title! please type the event title.");
+		return;
+	}
+
+	var type = "";
+	var start = document.getElementById('new-event-start').value;
+	var end = document.getElementById('new-event-end').value;
+	var notes = document.getElementById('new-event-notes').value;
+
+	if(document.getElementById('new-event-type-r1').checked) {
+		type = document.getElementById('new-event-type-r1').value;
+	} else if(document.getElementById('new-event-type-r2').checked) {
+		type = document.getElementById('new-event-type-r2').value;
+	} else if(document.getElementById('new-event-type-r3').checked) {
+		type = document.getElementById('new-event-type-r3').value;
+	}
+	var id = month[selectedDay.week][selectedDay.day].events.length;
+	var newEvent = new Event(id, name, type, start, end, notes);
+	month[selectedDay.week][selectedDay.day].events.push(newEvent);
+
+	resetNewEventModalInput();
+	modalCloseClick(document.getElementById('modal_close'));
+	renderEventListDOM(month[selectedDay.week][selectedDay.day]);
+
+}
+
+
+var resetNewEventModalInput = function() {
+	document.getElementById('new-event-name').value = "";
+	document.getElementById('new-event-start').value = "";
+	document.getElementById('new-event-end').value = "";
+	document.getElementById('new-event-notes').value = "";
+	document.getElementById('new-event-type-r1').checked = true;
+	document.getElementById('new-event-type-r2').checked = false;
+	document.getElementById('new-event-type-r3').checked = false;
+}
+
+var resetDOM = function() {
 	var eventListContainer = document.getElementById('event-list-container');
 	var monthNav = document.getElementById('month-nav');
 	var calendarContainer = document.getElementById('calendar-container');
